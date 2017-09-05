6816b1bba9ef0e6dd31f9ad50c2bdb137f01b906
Michal Altair Valášek <michal.valasek@altairis.cz>
2017-09-06 01:58:39 +0200

    Saving order to database

--- Altairis.ShirtShop.Web/Pages/Index.cshtml.cs
+++ Altairis.ShirtShop.Web/Pages/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Altairis.ShirtShop.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Altairis.ShirtShop.Web.Pages {
     public class IndexModel : PageModel {
@@ -15,10 +16,37 @@ namespace Altairis.ShirtShop.Web.Pages {
             this.context = dc;
         }
 
-        public IEnumerable<ShirtSize> ShirtSizes => this.context.ShirtSizes.OrderBy(x => x.SortPriority);
+        public IEnumerable<SelectListItem> ShirtSizes => this.context.ShirtSizes
+            .OrderBy(x => x.SortPriority)
+            .Select(x => new SelectListItem {
+                Text = $"{x.Name} ({x.Price} Kè)",
+                Value = x.Id.ToString()
+            });
 
-        public void OnGet() {
+        public IEnumerable<SelectListItem> ShirtModels => this.context.ShirtModels
+            .OrderBy(x => x.SortPriority)
+            .Select(x => new SelectListItem {
+                Text = x.Name,
+                Value = x.Id.ToString()
+            });
 
+        public IEnumerable<SelectListItem> DeliveryMethods => this.context.DeliveryMethods
+            .OrderBy(x => x.SortPriority)
+            .Select(x => new SelectListItem {
+                Text = $"{x.Name} ({x.Price} Kè)",
+                Value = x.Id.ToString()
+            });
+
+        [BindProperty]
+        public Order Order { get; set; }
+
+        public async Task<IActionResult> OnPostAsync() {
+            if (!this.ModelState.IsValid) return this.Page();
+
+            await this.context.Orders.AddAsync(this.Order);
+            await this.context.SaveChangesAsync();
+
+            return this.RedirectToPage("OK", new { OrderId = this.Order.Id });
         }
 
     }
